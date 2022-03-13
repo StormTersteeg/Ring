ad5f7f0947f055fc6224574d1e0a63c3d4bd3fa5
storm <stormtersteeglive@hotmail.com>
2022-03-13 16:07:01 +0100

    Now displaying current ip:port

--- Ring-120322/Program.cs
+++ Ring-120322/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Ring_120322
@@ -13,6 +15,16 @@ namespace Ring_120322
     {
         public static void Main(string[] args)
         {
+            // GET IPV4 OF CURRENT MACHINE
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            Console.WriteLine($"Clients can connect to: https://{localIP}:5001\n");
+
             CreateHostBuilder(args).Build().Run();
         }
 
